a518dd2771d1cd3d5437413f73cb9c5b6095be4c
Adrian Teran <153938220+aadriann12@users.noreply.github.com>
2024-06-07 16:09:33 +0200

    Update Mavenproject1.java

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello yje6j56j !");
+        System.out.println("Hello yjghghghfe6j56j !");
     }
 }
